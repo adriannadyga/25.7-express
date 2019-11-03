e1cd33cfc8dde38ca7a934094a39171596d4be64
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-03 10:59:07 +0100

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '487311514803-vg46bopmfn7u09j32sq4nf5pg1tm31br.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 's6Xha3_u5V80524ym4ArSEwp',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
